ae8c2d2624000df31e6743d6cb16ec4e04fbe2a7
JX170922 <HASEE@DESKTOP-OO424HK>
2018-06-15 16:19:15 +0800

    登陆ajax 时间:2018.6.15

--- src/com/health/controller/ControlInvoke.java
+++ src/com/health/controller/ControlInvoke.java
@@ -1,25 +1,24 @@
 package com.health.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.health.biz.ImplLoginBiz;
-import com.health.biz.IntLoginBiz;
-import com.health.entity.Worker;
-import com.health.entity.WorkerExample;
-import com.health.entity.WorkerExample.Criteria;
 import com.health.biz.ImplAccountMg;
+import com.health.biz.ImplLoginBiz;
 import com.health.entity.Account;
+import com.health.entity.Worker;
 
 @Controller
 public class ControlInvoke {
 
-	
 	@Resource
 	ImplAccountMg ImplAccountMg;
 
@@ -30,16 +29,17 @@ public class ControlInvoke {
 	 */
 
 	@Resource
-	private ImplLoginBiz impLoginBiz;   //后端接口-用于实现登陆
-	
+	private ImplLoginBiz impLoginBiz; // 后端接口-用于实现登陆
+
 	@RequestMapping("invokeTest.action")
 	public ModelAndView invokeTest() {
 		ModelAndView mav = new ModelAndView("jsp/index");
 		return mav;
 	}
-	
+
 	/*
 	 * 主界面点击系统设置->“团检单位设置”选项的时候请求的页面
+	 * 
 	 * @author罗杭春，6月14 日
 	 */
 	@RequestMapping("AccountMg.action")
@@ -52,7 +52,7 @@ public class ControlInvoke {
 	}
 
 	/*
-	 * 时间：2018.6.14 后台登陆跳转
+	 * 时间：2018.6.14 后台界面显示
 	 */
 	@RequestMapping("loginJsp.action")
 	public ModelAndView loginJsp() {
@@ -60,22 +60,40 @@ public class ControlInvoke {
 		ModelAndView mav = new ModelAndView("jsp/loginBackend");
 		return mav;
 	}
-	/*
-	 * 时间：2018.6.14 后台登陆界面显示
-	 */
-	@RequestMapping("loginAdmin.action")
-	public ModelAndView loginAdmin(Worker worker) {
-		System.out.println(worker.getName()+""+worker.getPassword());
-		   Worker login= impLoginBiz.loginAdmin(worker);
-		   if(login!=null) {
-			   
-			   ModelAndView mav = new ModelAndView("jsp/index");
-			   
-			   return mav;
-		   }
-		   return null;
-		
-	}
 
-  
+	 /*
+	 * 时间：2018.6.14 后台登陆跳转
+	 * 毛聪
+	 */
+	 @RequestMapping("loginAdmin.action")
+	 public ModelAndView loginAdmin(Worker worker) {
+	 System.out.println(worker.getName()+""+worker.getPassword());
+	 Worker login= impLoginBiz.loginAdmin(worker);
+	 if(login!=null) {
+	
+	 ModelAndView mav = new ModelAndView("jsp/index");
+	
+	 return mav;
+	 }
+	 return null;
+	
+	 }
+//	@RequestMapping("loginAdmin.action")
+//	public String loginAdmin(HttpServletResponse response,Worker worker) throws IOException {
+//		System.out.println(worker.getName() + "" + worker.getPassword());
+//		Worker login = impLoginBiz.loginAdmin(worker);
+//		PrintWriter printWriter = response.getWriter();
+//		if (login != null) {
+//			printWriter.print("OK");
+//			printWriter.flush();
+//			printWriter.close();
+//
+//			System.out.println("走到了admin");
+//		
+//		}
+//
+//		return "FAIL";
+//
+//	}
+
 }
